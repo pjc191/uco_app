3271def5246250765ac1a12be6f13c0e2be67c28
pjc191 <EuphoriaComplex@Prashawns-MacBook-Air.local>
2014-08-11 14:18:41 -0700

    Modified the models artist.rb and draft.rb

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,5 @@
 class Artist < ActiveRecord::Base
+	has_many :influencee_drafts, :class_name => "Draft", :foreign_key => "influencee_artist_id"
+	has_many :influencer_drafts, :class_name => "Draft", :foreign_key => "influencer_artist_id"
+
 end
--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,2 +1,4 @@
 class Draft < ActiveRecord::Base
+	belongs_to :influencee_artist, :class_name => "Artist"
+	belongs_to :influencer_artist, :class_name => "Artist"
 end
